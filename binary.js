100674e0f0b0449e0b7bd326ad9fcb386616d550
Emma-Nangida <emmanangida@gmail.com>
2023-04-20 20:51:15 +0300

    first commit

---
+++ binary.js
@@ -0,0 +1,28 @@
+//write javascript program to find positions of 1,5,9,4 in binary search
+function binary(items, value){
+    var firstIndex  = 0,
+        lastIndex   = items.length - 1,
+        middleIndex = Math.floor((lastIndex + firstIndex)/2);
+
+    while(items[middleIndex] != value && firstIndex < lastIndex)
+    {
+       if (value < items[middleIndex])
+        {
+            lastIndex = middleIndex - 1;
+        } 
+      else if (value > items[middleIndex])
+        {
+            firstIndex = middleIndex + 1;
+        }
+        middleIndex = Math.floor((lastIndex + firstIndex)/2);
+    }
+
+ return (items[middleIndex] != value) ? -1 : middleIndex;
+}
+var items = [1, 2, 3, 4, 5, 7, 8, 9];
+console.log(binary(items, 1));   
+console.log(binary(items, 5));
+console.log(binary(items, 9));
+console.log(binary(items, 4));
+
+
